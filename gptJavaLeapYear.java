6236ba123dfe68634eb9abff8b37b877c60fa684
aibelleonardo <168248615+aibelleonardo@users.noreply.github.com>
2024-06-03 23:48:48 +0800

    Add files via upload

---
+++ gptJavaLeapYear.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class LeapYearChecker {
+    public static boolean isLeapYear(int year) {
+        // Leap year is divisible by 4 and not divisible by 100
+        // unless it is also divisible by 400
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a year: ");
+        int year = scanner.nextInt();
+
+        if (isLeapYear(year)) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
+
+        scanner.close();
+    }
+}
